1106faf2672b7a545e9052ced5df16b244859d2d
Daniel Yang <danielwzyang@gmail.com>
2024-10-03 23:58:45 -0400

    further vw vs % issues; added filler team cards

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,9 +1,69 @@
 import Navbar from "@/components/ui/navbar"
+import TeamCard from "@/components/ui/teamcard"
+import daniel from "../../public/daniel.png"
+import profile from "../../public/profile.png"
 
 export default function Team() {
     return (
         <div>
             <Navbar />
+            <div className="mt-5 flex flex-wrap justify-center mx-auto w-[1200px] max-w-[90%] gap-5">
+                <TeamCard
+                    name="Daniel Yang"
+                    image={profile}
+                    position="president"
+                    bio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae qui sunt numquam iure iusto omnis minima libero incidunt. Reprehenderit, sequi."
+                    facebook="Daniel Yang"
+                    instagram="danielwyang"
+                    mail="danielwzyang@gmail.com"
+                />
+                <TeamCard
+                    name="Daniel Yang"
+                    image={profile}
+                    position="president"
+                    bio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae qui sunt numquam iure iusto omnis minima libero incidunt. Reprehenderit, sequi."
+                    facebook="Daniel Yang"
+                    instagram="danielwyang"
+                    mail="danielwzyang@gmail.com"
+                />
+                <TeamCard
+                    name="Daniel Yang"
+                    image={profile}
+                    position="president"
+                    bio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae qui sunt numquam iure iusto omnis minima libero incidunt. Reprehenderit, sequi."
+                    facebook="Daniel Yang"
+                    instagram="danielwyang"
+                    mail="danielwzyang@gmail.com"
+                />
+                <TeamCard
+                    name="Daniel Yang"
+                    image={profile}
+                    position="president"
+                    bio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae qui sunt numquam iure iusto omnis minima libero incidunt. Reprehenderit, sequi."
+                    facebook="Daniel Yang"
+                    instagram="danielwyang"
+                    mail="danielwzyang@gmail.com"
+                />
+                <TeamCard
+                    name="Daniel Yang"
+                    image={profile}
+                    position="president"
+                    bio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae qui sunt numquam iure iusto omnis minima libero incidunt. Reprehenderit, sequi."
+                    facebook="Daniel Yang"
+                    instagram="danielwyang"
+                    mail="danielwzyang@gmail.com"
+                />
+                <TeamCard
+                    name="Daniel Yang"
+                    image={profile}
+                    position="president"
+                    bio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae qui sunt numquam iure iusto omnis minima libero incidunt. Reprehenderit, sequi."
+                    facebook="Daniel Yang"
+                    instagram="danielwyang"
+                    mail="danielwzyang@gmail.com"
+                />
+
+            </div>
         </div>
     )
 }
--- components/ui/aboutsection.tsx
+++ components/ui/aboutsection.tsx
@@ -9,7 +9,7 @@ export interface sectionProps {
 
 export default function Section(props: sectionProps) {
     return <div className={"flex flex-wrap items-center" + (props.side == 1 ? "" : " sm:flex-row-reverse")} >
-        <div className="w-full h-[400px] sm:w-[50vw] sm:aspect-[5/4] flex items-center">
+        <div className="w-full h-[400px] sm:w-[50%] sm:aspect-[5/4] flex items-center">
             <Paragraph title={props.title} lines={props.lines} side={props.side} />
         </div>
         <div className="w-full sm:w-[50%] aspect-[5/4]"> <Image src={"/" + props.title + "jpg"} alt={props.title} width={960} height={768}/> </div>
