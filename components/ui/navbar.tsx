b261fe3aacc80319c34eaa6cd2a8b4375d36639b
Daniel Yang <danielwzyang@gmail.com>
2024-10-04 22:09:04 -0400

    rotating mobile navbar menu button

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -4,8 +4,12 @@ import Link from "next/link"
 import { Sheet, SheetTrigger, SheetContent, SheetTitle, SheetDescription } from "./sheet"
 import { Menu } from "lucide-react"
 import Image from "next/image"
+import * as motion from "framer-motion/client"
+import { useState } from "react"
 
 export default function Navbar() {
+    const [opened, toggleMenu] = useState(false)
+
     return <div className="h-[80px] relative flex items-center px-5">
         <Link href="/"><Image src="/sda.png" alt="icon" height={40} width={40} /></Link>
         <div className="hidden sm:flex gap-7 absolute right-10">
@@ -15,9 +19,14 @@ export default function Navbar() {
             <Link href="/team" className="text-2xl hover:text-[#9e9e9e]">team</Link>
             <Link href="/contact" className="text-2xl hover:text-[#9e9e9e]">contact</Link>
         </div>
-        <Sheet>
+        <Sheet onOpenChange={(open) => toggleMenu(open)}>
             <SheetTrigger className="h-10 w-10 flex justify-center items-center absolute right-5 sm:hidden">
-                <Menu />
+                <motion.div
+                    initial={{ rotate: 0 }}
+                    animate={opened ? { rotate: 180 } : { rotate: 0 }}
+                    transition={{ duration: 0.5 }}>
+                    <Menu />
+                </motion.div>
             </SheetTrigger>
             <SheetContent className="mt-20 w-fit h-fit bg-transparent border-0 p-0 pr-8" onOpenAutoFocus={(e) => e.preventDefault()}>
                 <SheetTitle></SheetTitle>
